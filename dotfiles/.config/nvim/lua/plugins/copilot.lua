880d999da42fe027f418567b78c27102a8976568
xismy <iye1994@gmail.com>
2025-06-01 20:07:01 +0200

    Configure copilot for manual trigger only

--- dotfiles/.config/nvim/lua/plugins/copilot.lua
+++ dotfiles/.config/nvim/lua/plugins/copilot.lua
@@ -1,3 +1,16 @@
 return {
-	"github/copilot.vim"
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	event = "VeryLazy",
+	opts = {
+		suggestion = {
+			enabled = true,
+			auto_trigger = false,
+			keymap = {
+				accept = "<C-l>",
+				next = "<C-j>",
+				prev = "<C-k>",
+			},
+		},
+	}
 }
