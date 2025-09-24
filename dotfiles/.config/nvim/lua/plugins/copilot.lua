197d978940f4ac1214e7d76572cd500739ba2e21
xismy <iye1994@gmail.com>
2025-09-24 15:28:51 +0200

    nvim: Copilot chat

--- dotfiles/.config/nvim/lua/plugins/copilot.lua
+++ dotfiles/.config/nvim/lua/plugins/copilot.lua
@@ -1,16 +1,27 @@
 return {
-	"zbirenbaum/copilot.lua",
-	cmd = "Copilot",
-	event = "VeryLazy",
-	opts = {
-		suggestion = {
-			enabled = true,
-			auto_trigger = false,
-			keymap = {
-				accept = "<C-l>",
-				next = "<C-j>",
-				prev = "<C-k>",
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "VeryLazy",
+		opts = {
+			suggestion = {
+				enabled = true,
+				auto_trigger = false,
+				keymap = {
+					accept = "<C-l>",
+					next = "<C-j>",
+					prev = "<C-k>",
+				},
 			},
+		}
+	},
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		dependencies = {
+			"zbirenbaum/copilot.lua",
+			{"nvim-lua/plenary.nvim", branch = "master"},
 		},
+		build = "make tiktoken",
+		opts = {},
 	}
 }
